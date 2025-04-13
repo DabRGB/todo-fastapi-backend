1d96dc62863e937c8a11268e737a3143aa32954c
DabRGB <clavite.dave09@gmail.com>
2025-04-13 21:47:31 +0800

    Fix import code2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 import models
 import schemas  
 import crud
-from .database import SessionLocal, engine
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
